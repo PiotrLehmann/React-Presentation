eb410b5d213ebf4c56b92eade51ec8b511865596
PiotrLehmann <piterrubik@gmail.com>
2023-03-28 15:59:03 +0200

    small changes on handle

--- app/src/WhyReact.js
+++ app/src/WhyReact.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import { InView } from "react-intersection-observer";
-import { motion } from "framer-motion";
+import { motion, useTransform } from "framer-motion";
 import OneLiner from "./OneLiner";
 
 const WhyReact = () => {
   const [inView, setInView] = useState(false);
-  const [isOn, setIsOn] = useState(true);
+  const [isOn, setIsOn] = useState(false);
   const toggleSwitch = () => {
     setIsOn(!isOn);
   };
@@ -25,40 +25,54 @@ const WhyReact = () => {
     opacity: 1,
   };
 
+  const changeForRed = {
+    background: 'linear-gradient(45deg, rgb(109, 23, 23), red)',
+  };
+
+  const changeForGreen = {
+    background: 'linear-gradient(45deg,rgb(14, 136, 24), #00ff0d)',
+  };
+
   return (
     <InView onChange={setInView}>
       {({ inView, ref }) => (
         <section className="content" id="why-react">
-          <div className="pros">
-            <motion.div className="pros-container" animate={isOn ? pop : hide}
+          <motion.div
+            className="pros"
+            animate={!isOn ? pop : hide}
             transition={spring}
-            >
+          >
+            <div className="pros-container">
               <OneLiner content="Performs fast" />
               <OneLiner content="Defines how data must be written - clear code, because of syntax standards" />
               <OneLiner content="Less coding" />
               <OneLiner content="UI renders faster due tu Virtual DOM" />
               <OneLiner content="Massive open-source environment" />
               <OneLiner content="Extensive collection of UI resusable components" />
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
           <div className="switch" data-isOn={isOn} onClick={toggleSwitch}>
             <motion.div
+            ref={ref}
               className="handle"
               layout
               transition={spring}
+              animate={isOn ? changeForRed: changeForGreen}
             ></motion.div>
           </div>
-          <div className="cons">
-            <motion.div className="cons-container" animate={!isOn ? pop : hide}
+          <motion.div
+            className="cons"
+            animate={isOn ? pop : hide}
             transition={spring}
-            >
+          >
+            <div className="cons-container">
               <OneLiner className="pro" content="High pace of development" />
               <OneLiner content="Performs fast, but slower than Vanilla JS" />
               <OneLiner content="Steep learning curve" />
               <OneLiner content="Covers only UI Layers of the app" />
               <OneLiner content="Syntax of JSX is harder than JS and creates some barriers" />
-            </motion.div>
-          </div>
+            </div>
+          </motion.div>
         </section>
       )}
     </InView>
